b80f11f9d608f57f845b5f0e96aa7b0bb3fcd1f6
gyusu han <slhopy@gmail.com>
2018-05-23 00:38:58 +0900

    delayed_reward_threshold 0.2에서 0.1로 변경

--- gyusu/main.py
+++ gyusu/main.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     # 강화학습 시작
     policy_learner = PolicyLearner(
         stock_code=stock_code, chart_data=chart_data, training_data=training_data,
-        min_trading_unit=1, max_trading_unit=2, delayed_reward_threshold=.2, lr=.001)
+        min_trading_unit=1, max_trading_unit=2, delayed_reward_threshold=.1, lr=.001)
     policy_learner.fit(balance=10000000, num_epoches=1000,
                        discount_factor=0, start_epsilon=.5)
 
